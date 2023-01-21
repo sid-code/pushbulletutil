77bf007d8501d1aba3c0ae1512d61a39c0743b9a
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2023-01-20 23:48:03 -0800

    Add flake

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  description = "Pushbullet utilities";
+
+  outputs = { self, nixpkgs }:
+
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      accessToken = "o.SffoPQY7HRazQC9iS3ea5iyC9m9ewFxJ";
+      url = "https://api.pushbullet.com/v2";
+      preamble =
+        ''
+          curl --silent \
+               --header 'Access-Token: ${accessToken}' \
+               --header 'Content-Type: application/json' '';
+      pushPreamble =
+        ''
+          ${preamble} \
+            --request POST \
+            --data-binary @- \
+            ${url}/pushes'';
+    in
+    {
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.pbpush;
+      packages.x86_64-linux.pbpush =
+        pkgs.writeShellScriptBin "pbpush" ''
+          ${pushPreamble} <<EOF
+          ${builtins.toJSON {
+            title = "$1";
+            body = "$2";
+            type = "note";
+          }}
+          EOF
+        '';
+
+      packages.x86_64-linux.pbpushto =
+        pkgs.writeShellScriptBin "pbpushto" ''
+          ${pushPreamble} <<EOF
+          ${builtins.toJSON {
+            iden = "$1";
+            title = "$2";
+            body = "$3";
+            type = "note";
+          }}
+          EOF
+        '';
+
+      packages.x86_64-linux.pbpushchannel =
+        pkgs.writeShellScriptBin "pbpushchannel" ''
+          ${pushPreamble} <<EOF
+          ${builtins.toJSON {
+            channel_tag = "$1";
+            title = "$2";
+            body = "$3";
+            type = "note";
+          }}
+          EOF
+        '';
+
+      packages.x86_64-linux.pbdevices =
+        pkgs.writeShellScriptBin "pbpush" ''
+          ${preamble} ${url}/devices | \
+            ${pkgs.jq}/bin/jq -r ".devices | .[] | (.iden + \" \" + if .nickname? then .nickname else \"<unknown>\" end)"
+        '';
+    };
+}
